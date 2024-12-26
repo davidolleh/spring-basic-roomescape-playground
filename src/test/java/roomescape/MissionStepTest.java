d71eb12381a2d13d12d127c58de612be03cc241b
davidolleh <asherolleh@gmail.com>
2024-12-26 13:42:01 +0900

    <FEAT> 2단계 Test 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -4,9 +4,16 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.auth.AuthService;
+import roomescape.auth.LoginRequestDto;
+import roomescape.auth.TokenGenerator;
+import roomescape.member.Member;
+import roomescape.reservation.ReservationResponse;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +23,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
+
+    private final TokenGenerator tokenGenerator;
+
+    public MissionStepTest(
+            @Autowired TokenGenerator tokenGenerator
+    ) {
+        this.tokenGenerator = tokenGenerator;
+    }
 
     @Test
     void 일단계() {
@@ -32,7 +47,57 @@ public class MissionStepTest {
                 .extract();
 
         String token = response.headers().get("Set-Cookie").getValue().split(";")[0].split("=")[1];
-
         assertThat(token).isNotBlank();
+
+        ExtractableResponse<Response> checkResponse = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie("token", token)
+                .when().get("/login/check")
+                .then().log().all()
+                .statusCode(200)
+                .extract();
+
+        assertThat(checkResponse.body().jsonPath().getString("name")).isEqualTo("어드민");
+    }
+
+    @Nested
+    class 이단계_클라스 {
+        @Autowired AuthService authService;
+
+        @Test
+        void 이단계() {
+            LoginRequestDto loginRequestDto = new LoginRequestDto("admin@email.com", "password");
+            Member member = authService.loginByEmailAndPassword(loginRequestDto);
+            String token = tokenGenerator.generateAccessToken(member);  // 일단계에서 토큰을 추출하는 로직을 메서드로 따로 만들어서 활용하세요.
+
+            Map<String, String> params = new HashMap<>();
+            params.put("date", "2024-03-01");
+            params.put("time", "1");
+            params.put("theme", "1");
+
+            ExtractableResponse<Response> response = RestAssured.given().log().all()
+                    .body(params)
+                    .cookie("token", token)
+                    .contentType(ContentType.JSON)
+                    .post("/reservations")
+                    .then().log().all()
+                    .extract();
+
+            assertThat(response.statusCode()).isEqualTo(201);
+            assertThat(response.as(ReservationResponse.class).getName()).isEqualTo("어드민");
+
+            params.put("name", "브라운");
+
+            ExtractableResponse<Response> adminResponse = RestAssured.given().log().all()
+                    .body(params)
+                    .cookie("token", token)
+                    .contentType(ContentType.JSON)
+                    .post("/reservations")
+                    .then().log().all()
+                    .extract();
+
+            assertThat(adminResponse.statusCode()).isEqualTo(201);
+            assertThat(adminResponse.as(ReservationResponse.class).getName()).isEqualTo("브라운");
+        }
     }
 }
